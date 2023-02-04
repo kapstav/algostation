5c9ad75ab9453cb7cfeb31869b6bb397006fe11b
kapstav <ekapstav@msn.com>
2023-02-04 18:45:46 +0000

    incr 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 nums1=input('Enter numbers separated by spaces:').split()
 nums2=input('Enter numbers separated by spaces:').split()
 def getCommon(nums1, nums2):
@@ -6,7 +7,33 @@ def getCommon(nums1, nums2):
    return nums1[i]
  return -1
 print(getCommon(nums1, nums2))
-
+#-----------------------
 var1=[7,8,9,10]
 if 9 in var1:
   print('ahha')
+#--------------------
+numbers = [{"value": 3}, {"value": 1}, {"value": 4}]
+numbers = sorted(numbers, key=lambda x: x["value"])
+print(numbers)  # Output: [{"value": 1}, {"value": 3}, {"value": 4}]
+numbers = [5,7,9,3]
+numbers = sorted(numbers, key=lambda x: x)
+print(numbers)  # Output: [{"value": 1}, {"value": 3}, {"value": 4}]
+
+
+import math
+
+
+class redwick:
+
+  def isReachable(self, x: int, y: int) -> bool:
+    a = math.gcd(x, y)
+    while a % 2 == 0:
+      a //= 2
+    return a == 1
+
+
+p = redwick()
+print(p.isReachable(64, 71))
+'''
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
